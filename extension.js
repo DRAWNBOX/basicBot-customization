eca6f7785c0c391d73a8ca03b68e980ed974c675
DRAWNBOX <drawnbox@hotmail.com>
2015-08-23 15:04:15 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -62,7 +62,7 @@
       botName: "Bacon Bot",
       language: "english",
       chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
-      scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
+      scriptLink: "https://rawgit.com/DRAWNBOX/basicBot-customization/master/extension.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
