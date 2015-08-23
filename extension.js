0884efa9ce225a1d21a8c7f38480e220fa0200aa
DRAWNBOX <drawnbox@github.com/drawnbox>
2015-08-23 13:38:29 -0400

    I changed the link at the bottom

--- extension.js
+++ extension.js
@@ -126,6 +126,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://raw.githubusercontent.com/DRAWNBOX/basicBot/master/basicBot.js", extend);
 
 }).call(this);
